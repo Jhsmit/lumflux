79491ab51ce477ac2f668eb73fd06c16c174deb3
J.H. Smit <jhsmit@gmail.com>
2022-09-27 19:08:41 +0200

    towards genericsource

--- lumflux/sources.py
+++ lumflux/sources.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Optional, Any
+
 import pandas as pd
 import param
 
@@ -13,36 +15,61 @@ class Source(param.Parameterized):
 
     updated = param.Event()
 
-    def get(self) -> None:
-        raise NotImplementedError()
+    # def get(self) -> None:
+    #     raise NotImplementedError()
 
 
-class TableSource(Source):
+class GenericSource(Source):
 
-    tables = param.Dict(default={}, doc="Dictionary of tables (pd.DataFrames)")
+    content = param.Dict(default={})
 
-    hashes = param.Dict(default={}, doc="Dictionary of table hashes")
+    hashes = param.Dict(default={})
+
+    def make_room(self):
+        if self.max_items and self.content:
+            first_key = next(iter(self.content))
+            self.content.pop(first_key)
+
+    def set(self, item, name=None):
+        self.hashes[name] = self.hash_item(item)
+        self.content[name] = item
+
+        self.updated = True
+
+    def get(self, name: Optional[str] = None) -> Any:
+        name = name or next(iter(self.keys()))
+        return self.content.get(name)
+
+    def hash_item(self, item):
+        return hash(item)
+
+
+class TableSource(GenericSource):
+
+    # tables = param.Dict(default={}, doc="Dictionary of tables (pd.DataFrames)")
+    #
+    # hashes = param.Dict(default={}, doc="Dictionary of table hashes")
 
     _type = "table"
 
-    def get(self):
-        if len(self.tables) == 0:
-            return None
-        elif len(self.tables) == 1:
-            return next(iter(self.tables.values()))
+    # def get(self):
+    #     if len(self.tables) == 0:
+    #         return None
+    #     elif len(self.tables) == 1:
+    #         return next(iter(self.tables.values()))
+    #
+    #     else:
+    #         raise ValueError("TableSource has multiple tables, use `get_table`")
 
-        else:
-            raise ValueError("TableSource has multiple tables, use `get_table`")
-
-    def set(self, df: pd.DataFrame) -> None:
-        if len(self.tables) > 1:
-            raise ValueError(
-                "Can only use the `set` method when the number of tables is below 1. "
-                "Use `set_table`"
-            )
-
-        table = next(iter(self.tables.keys())) if self.tables else 'main'
-        self.set_table(table, df)
+    # def set(self, df: pd.DataFrame) -> None:
+    #     if len(self.tables) > 1:
+    #         raise ValueError(
+    #             "Can only use the `set` method when the number of tables is below 1. "
+    #             "Use `set_table`"
+    #         )
+    #
+    #     table = next(iter(self.tables.keys())) if self.tables else 'main'
+    #     self.set_table(table, df)
 
     def set_table(self, table: str, df: pd.DataFrame) -> None:
         """Adds a new dataframe to the source.
@@ -54,11 +81,16 @@ class TableSource(Source):
         Returns:
 
         """
-        table_hash = hash_dataframe(df)
-        self.hashes[table] = table_hash
-        self.tables[table] = df
 
-        self.updated = True
+        self.set(df, table)
+        # table_hash = hash_dataframe(df)
+        # self.hashes[table] = table_hash
+        # self.tables[table] = df
+        #
+        # self.updated = True
+
+    def hash_item(self, item):
+        return hash_dataframe(item)
 
     def get_table(self, table: str) -> pd.DataFrame:
         """Get dataframe from the source
@@ -69,9 +101,12 @@ class TableSource(Source):
         Returns:
 
         """
-        df = self.tables.get(table, None)
 
-        return df
+        return self.get(table)
+        #
+        # df = self.tables.get(table, None)
+        #
+        # return df
 
     def get_tables(self) -> list[str]:
         """Get a list of tables available on this source.
@@ -80,7 +115,7 @@ class TableSource(Source):
             The list of available tables on this source.
         """
 
-        return list(self.tables.keys())
+        return list(self.content.keys())
 
 
 
